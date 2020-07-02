494dad3b236bc4ca10b603cda7cdf3f03bb3f835
Bryce Speirs <baspeirs@gmail.com>
2020-07-01 23:12:24 -0400

    corrected player model and created a controller for player data (need to ask questions about redirection)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,28 +6,31 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    // Sending back a password, even a hashed password, isn't a good idea
-    res.json({
-      email: req.user.email,
-      id: req.user.id
-    });
-  });
+  // ================================= sign in code ===========================
+  // app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  //   // Sending back a password, even a hashed password, isn't a good idea
+  //   res.json({
+  //     email: req.user.email,
+  //     id: req.user.id
+  //   });
+  // });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", (req, res) => {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
+  app.post("/api/characters", (req, res) => {
+    db.Character.create({
+      name: req.body.name,
+      race: req.body.race,
+      age: req.body.profession,
+      score: req.body.score
     })
-      .then(() => {
-        res.redirect(307, "/api/login");
-      })
-      .catch(err => {
-        res.status(401).json(err);
-      });
+      // .then(() => {
+      //   res.redirect(307, "/api/login"); // ===> what does this do?
+      // })
+      // .catch(err => {
+      //   res.status(401).json(err);
+      // });
   });
 
   // Route for logging user out
